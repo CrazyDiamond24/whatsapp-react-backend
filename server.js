64364433d6e3ec1632b413215b75c796aa2c22c8
Itay komemi <itaykomemi8@gmail.com>
2023-07-20 10:54:30 +0300

    deleting uneccery data

--- server.js
+++ server.js
@@ -23,8 +23,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
-const reviewRoutes = require('./api/review/review.routes')
-const carRoutes = require('./api/car/car.routes')
 const {setupSocketAPI} = require('./services/socket.service')
 
 // routes
@@ -33,8 +31,6 @@ app.all('*', setupAsyncLocalStorage)
 
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
-app.use('/api/review', reviewRoutes)
-app.use('/api/car', carRoutes)
 setupSocketAPI(http)
 
 // Make every server-side-route to match the index.html
