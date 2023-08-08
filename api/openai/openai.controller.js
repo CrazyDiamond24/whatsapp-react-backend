9c1bf9a08ac1b9e46e575a4262b7ae7a74c3d451
rania <raniakittan@hotmail.com>
2023-08-08 16:20:48 +0300

    profile ui done, added animations, fixed chatgpt

--- api/openai/openai.controller.js
+++ api/openai/openai.controller.js
@@ -3,15 +3,15 @@ const logger = require('../../services/logger.service')
 
 async function askGpt(req, res) {
   try {
-    const msg = req.body
-    console.log('prompt in req', msg)
-    const reply = await openaiService.ask(msg)
+    const { prompt, character } = req.body
+    const reply = await openaiService.ask(prompt, character)
     res.send(reply)
   } catch (err) {
     logger.error('Failed to get user', err)
     res.status(500).send({ err: 'Failed to get user' })
   }
 }
+
 module.exports = {
   askGpt,
 }
